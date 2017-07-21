5db3020f64385da717b46e3d70145b6df66c5789
knexho <ho_brian@me.com>
2017-07-21 02:02:24 -0400

    Adding in naming system.

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ SECRET_KEY = 'a secret key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-'''
 # CONNECTING TO POSTGRES
 conn_string = "host='localhost' dbname='bench_buddy' user='brianho' password=''"
 print "Connecting to database\n	-> %s" % (conn_string)
@@ -31,6 +30,8 @@ conn = psycopg2.connect(
     port=url.port
 )
 
+'''
+
 # conn.cursor will return a cursor object, you can use this cursor to perform queries
 cursor = conn.cursor()
 print "Connected!\n"
@@ -45,9 +46,11 @@ def test_reponse():
     # Track the conversation
     greeted = session.get("greeted", False)
     located = session.get("located", False)
+    bench_id = session.get("bench", -1)
+    named = session.get("named", False)
 
     body = request.values.get('Body').encode('utf-8')
-    print 'User message: "%s" (greeted: %r, located: %r)' % (body, greeted, located)
+    print 'User message: "%s" (greeted: %r, located: %r, bench_id: %r, named: %r)' % (body, greeted, located, bench_id, named)
 
     # If first time user, send greeting and instructions
     if not greeted:
@@ -63,6 +66,7 @@ def test_reponse():
             body = "%s Boston" % (body)
 
         # Check to see if user response is a place on Google Maps
+        print "Finding user ..."
         r = gmaps.places(body)
 
         # Can't find user response as a location
@@ -81,8 +85,9 @@ def test_reponse():
             print "Found user at %s -- %s" % (r['results'][0]['formatted_address'], map_url)
 
             # Make query to database
-            query = "SELECT id, street, park, lon, lat FROM benches WHERE ST_DWithin(st_setsrid(st_makepoint(%(lon_)s,%(lat_)s),4326), geom, .004) ORDER BY geom <-> st_setsrid(st_makepoint(%(lon_)s,%(lat_)s),4326) LIMIT 100;"
+            query = "SELECT id, street, park, lon, lat, name FROM benches WHERE ST_DWithin(st_setsrid(st_makepoint(%(lon_)s,%(lat_)s),4326), geom, .004) ORDER BY geom <-> st_setsrid(st_makepoint(%(lon_)s,%(lat_)s),4326) LIMIT 25;"
             cursor.execute(query, {"lon_":lon, "lat_":lat})
+            print "Querying database for nearby benches ..."
 
             # If there are no benches nearby
             if cursor.rowcount == 0:
@@ -91,12 +96,14 @@ def test_reponse():
 
             # If there are benches
             else:
+                print "Found some benches ..."
                 # Retrieve data from database query results
                 benches = []
-                for id_, street_, park_, lon_, lat_ in cursor:
-                    benches.append({"id": id_,"street": street_, "park": park_, "lon":lon_, "lat":lat_})
+                for id_, street_, park_, lon_, lat_, name_ in cursor:
+                    benches.append({"id": id_,"street": street_, "park": park_, "lon":lon_, "lat":lat_, "name":name_})
 
                 # Get Google Maps walking distance matrix for query results
+                print "Asking Google for distances ..."
                 r = gmaps.distance_matrix(origins="%f,%f" % (lat,lon), destinations=[(bench["lat"],bench["lon"]) for bench in benches], mode="walking", units="imperial")
 
                 # Find the nearest by walking
@@ -114,9 +121,12 @@ def test_reponse():
                 m = "Closest bench is"
                 bench = benches[min_index]
 
+                if bench["name"]:
+                    m += " %s" % (bench["name"])
+
                 # Add descriptions of landmarks and identifiers, if applicable
                 if bench["street"] != -1:
-                    query = "SELECT name, type FROM streets WHERE id = " + str(bench["street"])
+                    query = "SELECT name, type FROM streets WHERE id = %i" % (bench["street"])
                     cursor.execute(query)
                     street = cursor.fetchone()
 
@@ -127,7 +137,7 @@ def test_reponse():
                     m += " %s" % (bench["street_type"].title()) if bench["street_type"] != "" else ""
 
                 if bench["park"] != 0:
-                    query = "SELECT name FROM parks WHERE id = " + str(bench["park"])
+                    query = "SELECT name FROM parks WHERE id = %i" % (bench["park"])
                     cursor.execute(query)
                     park = cursor.fetchone()
 
@@ -135,27 +145,45 @@ def test_reponse():
 
                     m += " in %s" % (bench["park_name"])
 
+                if not bench["name"]:
+                    m += "\nWould you like to name this bench? Text a name, or text 'restart' to start over."
+
                 # Add distance to bench
-                map_url = short_url("https://www.google.com/maps/dir/?api=1&origin=%s,%s&destination=%s,%s&travelmode=walking" % (lat, lon, bench["lat"], bench["lon"]))
+                map_url = short_url("\nhttps://www.google.com/maps/dir/?api=1&origin=%s,%s&destination=%s,%s&travelmode=walking" % (lat, lon, bench["lat"], bench["lon"]))
 
                 m += " about %i ft and %s away to the %s!\n%s" % (bench["distance"], bench["duration"], ordinal(lon, bench["lon"], lat, bench["lat"]), map_url)
 
+                session["bench"] = bench["id"]
                 print "Found nearest bench -- %s" % (map_url)
-                '''
-                # Send bench location to user
-                resp = MessagingResponse()
-                resp.message(m)
 
-                # Construct follow-up message
-                m = "Would you like to find another bench? Text 'Y' to start over."
-                '''
-    elif greeted and located and "Y" not in body:
-        m = "I've already found you a bench. Text 'Y' to find another!"
+
+    elif greeted and located and named and "restart" not in body.lower():
+        m = "I've already found you a bench. Text 'restart' to find another!"
         print "Asking if they want to start over ..."
 
-    elif "y" or "reset" in body.lower():
+    elif greeted and located and "restart" not in body.lower():
+        try:
+            body.decode('ascii')
+        except UnicodeDecodeError:
+            m = "Simple names only, please! Try another name?"
+            print "Name not ASCII. Asking for another ..."
+        else:
+            if len(body) < 32:
+                query = "UPDATE benches SET name = %(name)s WHERE id = %(id)s;"
+                cursor.execute(query, ({"name":body.upper(), "id":session["bench"]}))
+
+                m = "Okay, that bench will be called %s!\nText 'restart' to find another! " % (body.upper())
+                session["named"] = True
+                print "Named bench %s" % (body.upper())
+            else:
+                m = "That name is too long. Try another name?"
+                print "Name was too long. Asking for another ..."
+
+    elif "restart" in body.lower():
         # session["greeted"] = False
         session["located"] = False
+        session["named"] = False
+        session["bench"] = -1
         m = "Okay! Where are you?"
         print "Starting session over ..."
 
