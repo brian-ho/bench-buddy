f8fc0424c5bf760bf5282f651afb5b60b58125cf
Michael Lawrence Evans <mlevans@users.noreply.github.com>
2017-08-15 15:44:17 -0400

    Update update-restrooms.sql

--- update-restrooms.sql
+++ update-restrooms.sql
@@ -1,8 +1,10 @@
 CREATE TABLE restrooms 
 (id integer, name text, address text, monday text, tuesday text, wednesday text,
 thursday text, friday text, saturday text, sunday text, lon double precision,
-lat double precision, geom geometry);
+lat double precision);
 
 \COPY restrooms FROM 'restrooms.csv' DELIMITER ',' CSV HEADER;
+
+ALTER TABLE "restrooms" ADD COLUMN "geom" geometry;
 
 UPDATE restrooms SET geom = ST_SetSRID(ST_MakePoint(lon,lat),4326);
